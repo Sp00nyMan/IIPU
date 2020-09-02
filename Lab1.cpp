a77d91875907876186e519331c446adbfe7f1aae
Anton Kreis <61134563+antonkreis@users.noreply.github.com>
2020-09-02 18:28:36 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -3,40 +3,43 @@
 #include <Windows.h>
 #include <setupapi.h>
 #include <iostream>
-#include <string.h>
+#include <regex>
+#include <iomanip>
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	std::cout << std::left << std::setw(120) << "Название" << std::left << std::setw(12) << "Vendor ID" << std::left << std::setw(12) << "Device ID" << std::endl;
 	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
 	if (devInfo == INVALID_HANDLE_VALUE)
 		return -1;
 	SP_DEVINFO_DATA devInfoData;
 	TCHAR buffer[1024];
 	DWORD a, b;
-	char *vendorID, *deviceID;
-
 	for (int i = 0; ; i++)
 	{
-		memset(&devInfoData, 0, sizeof(devInfoData));
 		devInfoData.cbSize = sizeof(devInfoData);
 		if (SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
 			break;
 		memset(buffer, 0, sizeof(buffer));
-		//SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, reinterpret_cast<BYTE*>(buffer), sizeof(buffer), &b);
-		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, sizeof(buffer), &b);
-		SetupDiDeleteDeviceInfo(devInfo, &devInfoData);
-		vendorID = strstr((char*)buffer, "Vid_") + 4;
-		//deviceID = strstr((char*)buffer, "Pid_") + 4;
-		//std::cout << devInfoData.ClassGuid << std::endl;
-		std::cout << (int)b << std::endl;
-		int j = 0;
-		while (j!=1024) {
-			std::cout << (char)buffer[j];
-			j++;
-		}
-		
-		//std::cout << vendorID << std::endl;
-		//std::cout << std::string(buffer, buffer + a) << std::endl;
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, 1024, &b);
+		std::wstring name(buffer);
+		//int p = name.find(L" - ");
+		//if(p != std::string::npos)
+		//	name = name.substr(0, p);
+
+		memset(buffer, 0, sizeof(buffer));
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_HARDWAREID, &a, (BYTE*)buffer, 1024, &b);
+		std::wstring ids(buffer);
+
+		std::wstring ven(ids.substr(ids.find(L"VEN_") + 4, 4));
+		std::wstring dev(ids.substr(ids.find(L"DEV_") + 4, 4));
+
+		//std::wcout << name.substr(name.size() - 4, 4) << '\t';
+		if (name.substr(name.size() - 4, 4) == dev)
+			name = name.substr(0, name.size() - 7);
+		std::wcout << std::left << std::setw(120) << name << "\t";
+		std::wcout << std::left << std::setw(12) << ven <<  std::left << std::setw(12) << dev << std::endl;
 	}
 	system("pause");
 }
