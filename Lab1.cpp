0db6a3e4f3c034e643d6cd05191c4e1768bd8ff7
Anton Kreis <61134563+antonkreis@users.noreply.github.com>
2020-09-02 10:29:39 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -3,23 +3,40 @@
 #include <Windows.h>
 #include <setupapi.h>
 #include <iostream>
+#include <string.h>
 
 int main()
 {
 	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
-	if(devInfo == INVALID_HANDLE_VALUE)
+	if (devInfo == INVALID_HANDLE_VALUE)
 		return -1;
 	SP_DEVINFO_DATA devInfoData;
 	TCHAR buffer[1024];
 	DWORD a, b;
-	
+	char *vendorID, *deviceID;
+
 	for (int i = 0; ; i++)
 	{
+		memset(&devInfoData, 0, sizeof(devInfoData));
 		devInfoData.cbSize = sizeof(devInfoData);
-		if(SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
+		if (SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
 			break;
-		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, reinterpret_cast<BYTE*>(buffer), sizeof(buffer), &b);
-		std::cout << std::string(buffer, buffer + a) << std::endl;
+		memset(buffer, 0, sizeof(buffer));
+		//SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, reinterpret_cast<BYTE*>(buffer), sizeof(buffer), &b);
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, sizeof(buffer), &b);
+		SetupDiDeleteDeviceInfo(devInfo, &devInfoData);
+		vendorID = strstr((char*)buffer, "Vid_") + 4;
+		//deviceID = strstr((char*)buffer, "Pid_") + 4;
+		//std::cout << devInfoData.ClassGuid << std::endl;
+		std::cout << (int)b << std::endl;
+		int j = 0;
+		while (j!=1024) {
+			std::cout << (char)buffer[j];
+			j++;
+		}
+		
+		//std::cout << vendorID << std::endl;
+		//std::cout << std::string(buffer, buffer + a) << std::endl;
 	}
 	system("pause");
 }
