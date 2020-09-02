52e383ce8af5c943501db58d432064be122dcd56
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-09-02 12:34:06 +0300

    Done

--- Lab1.cpp
+++ Lab1.cpp
@@ -3,25 +3,38 @@
 #include <Windows.h>
 #include <setupapi.h>
 #include <iostream>
+#include <regex>
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
 	if(devInfo == INVALID_HANDLE_VALUE)
 		return -1;
 	SP_DEVINFO_DATA devInfoData;
 	TCHAR buffer[1024];
 	DWORD a, b;
-	
 	for (int i = 0; ; i++)
 	{
 		devInfoData.cbSize = sizeof(devInfoData);
 		if(SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
 			break;
 		memset(buffer, 0, sizeof(buffer));
-		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, sizeof(buffer), &b);
-		//std::cout << "Len = " << int(b) << std::endl;
-		std::cout << std::string(buffer, buffer + b) << std::endl;
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, 1024, &b);
+		std::wstring name(buffer);
+		int p = name.find(L" - ");
+		if(p != std::string::npos)
+			name = name.substr(0, p);
+		std::wcout << name << "\t";
+
+		memset(buffer, 0, sizeof(buffer));
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_HARDWAREID, &a, (BYTE*)buffer, 1024, &b);
+		std::wstring ids(buffer);
+		
+		std::wstring ven(ids.substr(ids.find(L"VEN_") + 4, 4));
+		std::wstring dev(ids.substr(ids.find(L"DEV_") + 4, 4));
+
+		std::wcout << ven << '\t' << dev << std::endl;
 	}
 	system("pause");
 }
