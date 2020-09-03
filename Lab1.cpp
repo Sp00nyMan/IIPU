0c1b20e27ae85428762968f2d17635cbed4e4ae8
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-09-03 10:51:34 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -4,12 +4,14 @@
 #include <setupapi.h>
 #include <iostream>
 #include <regex>
+#include <iomanip>
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	std::cout << std::left << std::setw(120) << "Название" << std::left << std::setw(12) << "Vendor ID" << std::left << std::setw(12) << "Device ID" << std::endl;
 	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
-	if(devInfo == INVALID_HANDLE_VALUE)
+	if (devInfo == INVALID_HANDLE_VALUE)
 		return -1;
 	SP_DEVINFO_DATA devInfoData;
 	TCHAR buffer[1024];
@@ -17,27 +19,25 @@ int main()
 	for (int i = 0; ; i++)
 	{
 		devInfoData.cbSize = sizeof(devInfoData);
-		if(SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
+		if (SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
 			break;
 		memset(buffer, 0, sizeof(buffer));
 		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, 1024, &b);
 		std::wstring name(buffer);
-		//int p = name.find(L" - ");
-		//if(p != std::string::npos)
-		//	name = name.substr(0, p);
 
 		memset(buffer, 0, sizeof(buffer));
 		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_HARDWAREID, &a, (BYTE*)buffer, 1024, &b);
 		std::wstring ids(buffer);
-		
+
 		std::wstring ven(ids.substr(ids.find(L"VEN_") + 4, 4));
 		std::wstring dev(ids.substr(ids.find(L"DEV_") + 4, 4));
-		
-		//std::wcout << name.substr(name.size() - 4, 4) << '\t';
-		if(name.substr(name.size() - 4, 4) == dev)
+
+		if (name.substr(name.size() - 4, 4) == dev)
 			name = name.substr(0, name.size() - 7);
-		std::wcout << name << "\t";
-		std::wcout << ven << '\t' << dev << std::endl;
+		std::wcout << std::left << std::setw(120) << name << "\t";
+		std::wcout << std::left << std::setw(12) << ven <<  std::left << std::setw(12) << dev << std::endl;
+		SetupDiDeleteDeviceInfo(devInfo, &devInfoData);
 	}
+	SetupDiDestroyDeviceInfoList(devInfo);
 	system("pause");
 }
