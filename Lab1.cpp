64f2fcd424a91efacdd755e60f47508add08ff50
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-09-03 13:03:05 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -3,30 +3,31 @@
 #include <Windows.h>
 #include <setupapi.h>
 #include <iostream>
-#include <regex>
+//#include <regex>
 #include <iomanip>
-
+const char name_len = 100;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	std::cout << std::left << std::setw(120) << "Название" << std::left << std::setw(12) << "Vendor ID" << std::left << std::setw(12) << "Device ID" << std::endl;
-	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+	std::cout << std::left << std::setw(name_len) << "Название" << std::left << std::setw(12) << "Vendor ID" << std::left << std::setw(12) << "Device ID" << std::endl;
+	
+	HDEVINFO devInfo = SetupDiGetClassDevs(nullptr, TEXT("PCI"), nullptr, DIGCF_PRESENT | DIGCF_ALLCLASSES);
 	if (devInfo == INVALID_HANDLE_VALUE)
 		return -1;
 	SP_DEVINFO_DATA devInfoData;
 	TCHAR buffer[1024];
-	DWORD a, b;
+
 	for (int i = 0; ; i++)
 	{
 		devInfoData.cbSize = sizeof(devInfoData);
 		if (SetupDiEnumDeviceInfo(devInfo, i, &devInfoData) == FALSE)
 			break;
 		memset(buffer, 0, sizeof(buffer));
-		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, &a, (BYTE*)buffer, 1024, &b);
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_DEVICEDESC, nullptr, (BYTE*)buffer, 1024, nullptr);
 		std::wstring name(buffer);
 
 		memset(buffer, 0, sizeof(buffer));
-		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_HARDWAREID, &a, (BYTE*)buffer, 1024, &b);
+		SetupDiGetDeviceRegistryProperty(devInfo, &devInfoData, SPDRP_HARDWAREID, nullptr, (BYTE*)buffer, 1024, nullptr);
 		std::wstring ids(buffer);
 
 		std::wstring ven(ids.substr(ids.find(L"VEN_") + 4, 4));
@@ -34,7 +35,7 @@ int main()
 
 		if (name.substr(name.size() - 4, 4) == dev)
 			name = name.substr(0, name.size() - 7);
-		std::wcout << std::left << std::setw(120) << name << "\t";
+		std::wcout << std::left << std::setw(name_len) << name << "\t";
 		std::wcout << std::left << std::setw(12) << ven <<  std::left << std::setw(12) << dev << std::endl;
 		SetupDiDeleteDeviceInfo(devInfo, &devInfoData);
 	}
