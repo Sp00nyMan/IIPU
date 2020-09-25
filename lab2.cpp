d51dde42c5b4aa5bf9283fb1f3d7ad603453b6ab
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-09-25 13:17:38 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,17 +1,105 @@
 #include <Windows.h>
 #include <Setupapi.h>
-#include <Ntddstor.h>
 #pragma comment(lib, "setupapi.lib")
 
 #include <iostream>
-#include <regex>
-#include <iomanip>
-#include <stdlib.h>
-//#define WIN32_LEAN_AND_MEAN
+std::string busType[] = { "TypeUnknown",
+							"Scsi",
+							"Atapi",
+							"Ata",
+							"1394",
+							"Ssa",
+							"Fibre",
+							"Usb",
+							"RAID",
+							"Scsi",
+							"Sas",
+							"Sata",
+							"Sd",
+							"Mmc",
+							"Virtual",
+							"FileBackedVirtual",
+							"Spaces",
+							"Nvme",
+							"SCM",
+							"Ufs",
+							"Max",
+							"Reserved" };
 
+void getMemoryInfo()
+{
+	_ULARGE_INTEGER totalSpace;
+	_ULARGE_INTEGER freeSpace;
+	totalSpace.QuadPart = 0;
+	freeSpace.QuadPart = 0;
+
+	//Получаем битовую маску, представляющую имеющиеся в настоящие время дисковые накопители. 
+	const unsigned long presentMask = GetLogicalDrives();
+
+	for(char letter = 'A'; letter < 'Z'; ++letter)
+	{
+		if(presentMask >> letter - 'A')
+		{
+			std::string drive = std::string(1, letter) + ":\\";
+			
+			_ULARGE_INTEGER total;
+			_ULARGE_INTEGER free;
+			GetDiskFreeSpaceExA(drive.c_str(), 
+				nullptr, &total, &free);
+			total.QuadPart /= pow(1024, 2);
+			free.QuadPart /= pow(1024, 2);
+
+			//Определяем тип диска(3 - фиксированный диск) 
+			if (GetDriveTypeA(drive.c_str()) == DRIVE_FIXED)
+			{
+				totalSpace.QuadPart += total.QuadPart;
+				freeSpace.QuadPart += free.QuadPart;
+			}
+		}
+	}
+	std::cout << "Информация о пямяти [GB]: " << std::endl;
+	
+	std::cout << "\tВсего: " << totalSpace.QuadPart / 1024.0 << std::endl;
+	std::cout << "\tСвободно: " << freeSpace.QuadPart / 1024.0 << std::endl;
+	std::cout << "\tЗанято: " << (totalSpace.QuadPart - freeSpace.QuadPart) / 1024.0 << " ("<< 100 - (double)freeSpace.QuadPart / totalSpace.QuadPart * 100 << "%)" << std::endl;
+}
+
+int getDiskInfo(const char* name)
+{
+	STORAGE_PROPERTY_QUERY storagePropertyQuery;				//Структура с информацией об запросе 
+	storagePropertyQuery.QueryType = PropertyStandardQuery;		//Запрос драйвера, чтобы он вернул дескриптор устройства. 
+	storagePropertyQuery.PropertyId = StorageDeviceProperty;	//Флаг, гооврящий мы хотим получить дескриптор устройства. 
+	HANDLE diskHandle = CreateFileA((std::string("\\\\.\\") + name).c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+	if (diskHandle == INVALID_HANDLE_VALUE) {
+		return -1;
+	}
+
+	STORAGE_DEVICE_DESCRIPTOR* deviceDescriptor = static_cast<STORAGE_DEVICE_DESCRIPTOR*>(calloc(1024, 1));
+	if (!DeviceIoControl(diskHandle, IOCTL_STORAGE_QUERY_PROPERTY, &storagePropertyQuery, sizeof(storagePropertyQuery), deviceDescriptor, 1024, NULL, 0)) {
+		return GetLastError();
+	}
+
+
+	char *tmp = (char*)deviceDescriptor;
+
+	const std::string model(tmp + deviceDescriptor->ProductIdOffset);
+	std::cout << "Производитель: " << model.substr(0, model.find_first_of(' ')) << std::endl;
+	std::cout << "Модель: " <<  model.substr(model.find_first_of(' ') + 1) << std::endl;
+
+	std::cout << "Серийный номер устройства: " << tmp + deviceDescriptor->SerialNumberOffset << std::endl;
+	std::cout << "Версия прошивки (firmware): " << tmp + deviceDescriptor->ProductRevisionOffset << std::endl;
+	std::cout << "Тип шины: " << busType[deviceDescriptor->BusType] << std::endl;
+
+	CloseHandle(diskHandle);
+}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
+	getDiskInfo("PhysicalDrive0");
+	getMemoryInfo();
+	system("pause");
+	return 0;
+	/*
 	HANDLE devHandle = CreateFileA("\\\\.\\PhysicalDrive0", 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 	if (devHandle == INVALID_HANDLE_VALUE) {
 		return -1;
@@ -48,7 +136,7 @@ int main() {
 	storageDeviceDescriptor = (STORAGE_DEVICE_DESCRIPTOR*)outBuffer;
 
 	int offset = storageDeviceDescriptor->ProductIdOffset;
-	std::cout << "РќР°Р·РІР°РЅРёРµ СѓСЃС‚СЂРѕР№СЃС‚РІР°: ";
+	std::cout << "Название устройства: ";
 	while (outBuffer[offset] != '\0') {
 		std::cout << outBuffer[offset];
 		offset++;
@@ -56,7 +144,7 @@ int main() {
 	std::cout << std::endl;
 
 	offset = storageDeviceDescriptor->SerialNumberOffset;
-	std::cout << "РЎРµСЂРёР№РЅС‹Р№ РЅРѕРјРµСЂ СѓСЃС‚СЂРѕР№СЃС‚РІР°: ";
+	std::cout << "Серийный номер устройства: ";
 	while (outBuffer[offset+1] != '\0') {
 		std::cout << outBuffer[offset];
 		offset++;
@@ -64,7 +152,7 @@ int main() {
 	std::cout << std::endl;
 
 	offset = storageDeviceDescriptor->ProductRevisionOffset;
-	std::cout << "Р’РµСЂСЃРёСЏ РїСЂРѕС€РёРІРєРё (firmware): ";
+	std::cout << "Версия прошивки (firmware): ";
 	while (outBuffer[offset] != '\0') {
 		std::cout << outBuffer[offset];
 		offset++;
@@ -94,4 +182,5 @@ int main() {
 
 	system("pause");
 	return 0;
+	*/
 }
